3e6d56ba6f29501864926db546afe86cddf7c146
mancity142 <alanverghesemathew20@gmail.com>
2026-09-22 01:07:08 +0530

    add comments to this file

--- groupanagram/sorted.py
+++ groupanagram/sorted.py
@@ -1,8 +1,11 @@
 class Solution:
-    from typing import defaultdict,list
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result=defaultdict(list)
-        for s in strs:
-            sortedS=''.join(sorted(s))
-            result[sortedS].append(s)
-        return list(result.values())
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:  
+
+        result=defaultdict(list)            ## here creating a result dictionary to hold on to same keys and corresponding strings ##
+                          
+        for s in strs:                        # movving through the strs to find each s
+            sortedS=''.join(sorted(s))        # here we get a string after joining the sorted list
+               
+            result[sortedS].append(s)         # result = to the key pair append the string to get like {act:["cat"],["tac"]}
+
+        return list(result.values())          # return the values like [["act"],["posts"]]
