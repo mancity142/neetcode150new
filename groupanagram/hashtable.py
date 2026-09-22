c2c5e3ffe4b4432ff30eacc97530a50da3eb041c
mancity142 <alanverghesemathew20@gmail.com>
2026-09-23 00:13:11 +0530

    modified: hashtable.py with commends

--- groupanagram/hashtable.py
+++ groupanagram/hashtable.py
@@ -5,5 +5,7 @@ class Solution:
             count=[0]*26              ## array initialized to store upto 26 characters ##
             for c in s:                            # have each character in the word ##
                 count[ord(c)-ord('a')]+=1        
-            res[tuple(count)].append(s)
+            res[tuple(count)].append(s)      # here count is a list of 26 characters ex: [1,0,0,0,,0,,0,....] upto 26 and act as key here and to make it inmutable we use tuple ##
         return list(res.values())
+
+    
