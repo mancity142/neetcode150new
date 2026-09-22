9db9c287d3b91d6308eb9faf920e2e71ffa85b2c
mancity142 <alanverghesemathew20@gmail.com>
2026-09-22 23:51:41 +0530

    modified: hashtable.py with commends

--- groupanagram/hashtable.py
+++ groupanagram/hashtable.py
@@ -1,9 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res=defaultdict(list)
-        for s in strs:
-            count=[0]*26
-            for c in s:
-                count[ord(c)-ord('a')]+=1
+        res=defaultdict(list)          
+        for s in strs:                  
+            count=[0]*26              ## array initialized to store upto 26 characters ##
+            for c in s:                            # have each character in the word ##
+                count[ord(c)-ord('a')]+=1        
             res[tuple(count)].append(s)
         return list(res.values())
