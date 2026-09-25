f2a8260143b5bd305e74c35f4d9e7435452963a0
mancity142 <alanverghesemathew20@gmail.com>
2026-09-25 10:57:08 +0530

    added final  commands

--- kmostfrequency/optimal.py
+++ kmostfrequency/optimal.py
@@ -5,13 +5,13 @@ class Solution:
 
     for n in nums:
         count[n]=1+count.get(n,0)                ## here assigns the count for elements as ex: {1:3,2:2} etc
-    for n,c in count.items():
-        freq[c].append(n)   
-    res=[]
-    for i in range(len(freq)-1,0,-1):
-        for n in freq[i]:
-            res.append(n)
-            if len(res)==k:
+    for n,c in count.items():                    ## here we have the updation taking place in bucket sort array here n is the number and c is count ##
+        freq[c].append(n)                         
+    res=[]                                       ## creating a empty list to find result ##
+    for i in range(len(freq)-1,0,-1):            ## in the bucket sort array moving from the most frequent to lower is desc order ##
+        for n in freq[i]:                        ## iteration through the numbers in each frequency[index] ##
+            res.append(n)                        ## adding it to result ##
+            if len(res)==k:                       ## if length of the elements in res is more than k then reject only till equals it is possible ##
                 return res 
               
 
