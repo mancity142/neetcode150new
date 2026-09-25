6be8b7462ff146c7bec53fd12cab02a0df89f42d
mancity142 <alanverghesemathew20@gmail.com>
2026-09-26 01:59:57 +0530

    modified:   optimal.py

--- encode/optimal.py
+++ encode/optimal.py
@@ -5,13 +5,13 @@ class Solution:
             res+=len(str)+"#"+s         ## here we are looking for smth like ex:'4#look5#paste'
         return res
     def decode(self,s:str)->List[str]
-        res=[]
-        i=0
-        while i <len(s):
-            j=i
-            while s[j]!='#':
+        res=[]                           ## here creating a case where we have the res stored as a list ##
+        i=0                              ## setting pointers i =0 for start of the string
+        while i <len(s):                  # checking condition if i goes out of the len (of s) 
+            j=i                           # initilly in same position i,j
+            while s[j]!='#':                # here we have the case where the s[j] !='#' then move j=j+1
                 j+=1
-            length=int(s[i:j])
+            length=int(s[i:j])              # we require 
             res.append(s[j+1:j+1+length])
             i=j+1+length
         return res
