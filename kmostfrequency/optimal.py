0f230fefb21c2b9b2bbf1417564cc304972729ae
mancity142 <alanverghesemathew20@gmail.com>
2026-09-25 11:48:29 +0530

    added final  commands

--- kmostfrequency/optimal.py
+++ kmostfrequency/optimal.py
@@ -1,7 +1,7 @@
 class Solution:
     def topKFrequent(self,nums:List[int],k:int) -> List[int]:
     count={}                                      ## initializing a hashmap 
-    freq=[[] for i in range(len(nums)+1)]         ## sets up bucket sort array over here to have the array set with all numbers to n+1 ## 
+    freq=[[] for i in range(len(nums)+1)]         ## sets up bucket sort array( 2 D Array) over here to have the array set with all numbers to n+1 ## 
 
     for n in nums:
         count[n]=1+count.get(n,0)                ## here assigns the count for elements as ex: {1:3,2:2} etc
