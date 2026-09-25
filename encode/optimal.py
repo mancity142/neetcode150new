021f6b61616aa1f5489aaf820bc30a5ca09d1c25
mancity142 <alanverghesemathew20@gmail.com>
2026-09-26 01:54:37 +0530

    modified:   optimal.py

--- encode/optimal.py
+++ encode/optimal.py
@@ -1,12 +1,17 @@
 class Solution:
     def encode(self, strs: List[str]) -> str:
-        res=''
-        for s in str:
-            res+=len(str)+"#"+s
+        res=''                          ## creating a res variable to have string here ##
+        for s in str:                   ## iterating in s in strs ##
+            res+=len(str)+"#"+s         ## here we are looking for smth like ex:'4#look5#paste'
         return res
-    def encode(self,s:str)->List[str]
+    def decode(self,s:str)->List[str]
         res=[]
         i=0
-        while i<len(s):
+        while i <len(s):
             j=i
-            
+            while s[j]!='#':
+                j+=1
+            length=int(s[i:j])
+            res.append(s[j+1:j+1+length])
+            i=j+1+length
+        return res
