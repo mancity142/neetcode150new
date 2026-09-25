6ad5da7e6c59b7fe1163e85456fc672036cdf129
mancity142 <alanverghesemathew20@gmail.com>
2026-09-26 02:25:54 +0530

    modified:   optimal.py

--- encode/optimal.py
+++ encode/optimal.py
@@ -11,7 +11,7 @@ class Solution:
             j=i                           # initilly in same position i,j
             while s[j]!='#':                # here we have the case where the s[j] !='#' then move j=j+1
                 j+=1
-            length=int(s[i:j])              # we require to find the length of strimg here when j=# at first then we use this to append the string to the list
+            length=int(s[i:j])              # we require to find the length of strimg here when j=# at first then we use this to append the string to the list  -> here what happens is we find the length by slicing [0,1] in all cases
             res.append(s[j+1:j+1+length])    
             i=j+1+length                     # we increment the starting pointer to next word ie,p in paste
         return res
